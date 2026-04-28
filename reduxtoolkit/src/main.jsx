b3229b986b7775badc90c450bffb680e89e3a54a
vishwajit-nikam-programmer <vishwajitnamdeo749@gmail.com>
2026-04-28 12:37:18 +0530

    reduxtoolkit complete

---
+++ reduxtoolkit/src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {Provider} from 'react-redux'
+import { store } from './app/Store'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
